3fcbc6325d8edaa131b2108270d0b4052fad7679
Aleksandr Ivashchenko <aleksandr.ivashchenko@sigma.software>
2017-11-08 16:14:24 +0200

    master > Initial commit

---
+++ Quizapp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Quizapp
+//
+//  Created by Aleksandr Ivashchenko on 11/8/17.
+//  Copyright © 2017 Sigma Software. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+  
+}
+
